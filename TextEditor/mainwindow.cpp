a5c553ff8b0193cc0dc5a39a2f4ab1fbe744768b
chenyouhui1 <chenyouhui1@joyy.com>
2023-09-28 09:14:40 +0800

    文本编辑器 -- 连接菜单栏按钮槽

--- TextEditor/mainwindow.cpp
+++ TextEditor/mainwindow.cpp
@@ -9,19 +9,36 @@
 #include <QMenuBar>
 #include <QToolBar>
 #include <QStatusBar>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QTextStream>
+#include <functional>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
     createMenu();
+    init();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::init()
+{
+    // 初始化文件为未保存状态
+    isUntitled = true;
+    // 初始化文件名为"未命名.txt"
+    curFile = "未命名.txt";
+    // 初始化窗口标题为文件名
+    setWindowTitle(curFile);
+
+    initEvent();
 }
 
 void MainWindow::createMenu()
@@ -115,5 +132,108 @@ void MainWindow::createMenuActionBtn(QMenu* menu, QJsonValue& actionConf, bool& 
         toolbarSeparator = separator;
     }
 
+    // 连接槽
+    connect(actionBtn, &QAction::triggered, this, [this, name](bool trigger) {
+        onActionClick(trigger, name);
+    });
+}
+
+void MainWindow::onActionClick(bool trigger, QString name)
+{
+    qDebug() << name << " tri : " << trigger;
+    auto func = actionEvntMap.find(name);
+    if(func != actionEvntMap.end())
+    {
+        qDebug() << name << "  执行回调 ";
+    }else {
+        qDebug() << name << "  未注册回调方法 ";
+    }
+}
+
+void MainWindow::initEvent()
+{
+    actionEvntMap = {
+        {"new", std::bind(&MainWindow::newFile, this)},
+        {"save", std::bind(&MainWindow::save, this)},
+    };
+}
+
+bool MainWindow::newFile()
+{
+    if (maybeSave()) {
+        isUntitled = true;
+        curFile = tr("未命名.txt");
+        setWindowTitle(curFile);
+        ui->textEdit->clear();
+        ui->textEdit->setVisible(true);
+
+        return true;
+    }
+    return false;
+}
+
+bool MainWindow::maybeSave()
+{
+    // 如果文档被更改了
+    if (ui->textEdit->document()->isModified()) {
+    // 自定义一个警告对话框
+       QMessageBox box;
+       box.setWindowTitle(tr("警告"));
+       box.setIcon(QMessageBox::Warning);
+       box.setText(curFile + tr(" 尚未保存，是否保存？"));
+       QPushButton *yesBtn = box.addButton(tr("是(&Y)"),
+                        QMessageBox::YesRole);
+       box.addButton(tr("否(&N)"), QMessageBox::NoRole);
+       QPushButton *cancelBut = box.addButton(tr("取消"),
+                        QMessageBox::RejectRole);
+       box.exec();
+       if (box.clickedButton() == yesBtn)
+            return save();
+       else if (box.clickedButton() == cancelBut)
+            return false;
+   }
+   // 如果文档没有被更改，则直接返回true
+   return true;
+}
+
+bool MainWindow::save()
+{
+   if (isUntitled) {
+       return saveAs();
+   } else {
+       return saveFile(curFile);
+   }
+}
+
+bool MainWindow::saveAs()
+{
+   QString fileName = QFileDialog::getSaveFileName(this, "另存为 ", curFile);
+   if (fileName.isEmpty()) return false;
+   return saveFile(fileName);
+}
+
+bool MainWindow::saveFile(const QString &fileName)
+{
+   QFile file(fileName);
+
+   if (!file.open(QFile::WriteOnly | QFile::Text)) {
+
+       // %1和%2分别对应后面arg两个参数，/n起换行的作用
+       QMessageBox::warning(this, tr("多文档编辑器"),
+                            tr("无法写入文件 %1：/n %2")
+                  .arg(fileName).arg(file.errorString()));
+       return false;
+   }
+   QTextStream out(&file);
+   // 鼠标指针变为等待状态
+   QApplication::setOverrideCursor(Qt::WaitCursor);
+   out << ui->textEdit->toPlainText();
+   // 鼠标指针恢复原来的状态
+   QApplication::restoreOverrideCursor();
+   isUntitled = false;
+   // 获得文件的标准路径
+   curFile = QFileInfo(fileName).canonicalFilePath();
+   setWindowTitle(curFile);
+   return true;
 }
 
--- TextEditor/mainwindow.h
+++ TextEditor/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <map>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -24,5 +26,24 @@ private:
     void createMenu();
     void createMenuBtn(QJsonValue& menuConf);
     void createMenuActionBtn(QMenu* menu, QJsonValue& actionConf, bool& toolbarSeparator);
+
+    void onActionClick(bool trigger, QString name);
+
+private:
+    bool newFile();   // 新建操作
+    bool maybeSave(); // 判断是否需要保存
+    bool save();      // 保存操作
+    bool saveAs();    // 另存为操作
+    bool saveFile(const QString &fileName); // 保存文件
+    std::map<QString, std::function<bool()>> actionEvntMap;
+    void initEvent();
+
+private:
+    // 为真表示文件没有保存过，为假表示文件已经被保存过了
+    bool isUntitled;
+    // 保存当前文件的路径
+    QString curFile;
+    // 初始化
+    void init();
 };
 #endif // MAINWINDOW_H
